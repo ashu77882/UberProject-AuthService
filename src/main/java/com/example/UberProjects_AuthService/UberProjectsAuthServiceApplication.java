84fb67652052127fa78657382b27f14a8e6be3aa
Ashutosh <ashutoshmaddheshiya979@gmail.com>
2025-09-20 16:35:11 +0530

    Auth Service

---
+++ src/main/java/com/example/UberProjects_AuthService/UberProjectsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberProjects_AuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UberProjects_AuthService/UberProjectsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberProjects_AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
